6f8e331d3bab8bd5b54e75c03f771da61f8d45dc
Malik <hiraev.github@gmail.com>
2020-07-11 02:15:47 +0300

    Add main fragment with search Toolbar

--- app/src/main/kotlin/ru/khiraevmalik/theguardiannews/presentation/main/NewsListFragment.kt
+++ app/src/main/kotlin/ru/khiraevmalik/theguardiannews/presentation/main/NewsListFragment.kt
@@ -1,32 +1,77 @@
 package ru.khiraevmalik.theguardiannews.presentation.main
 
 import android.os.Bundle
-import android.os.Parcelable
-import android.util.Log
+import android.transition.Fade
+import android.transition.Transition
+import android.transition.TransitionManager
+import android.transition.Visibility
 import android.view.View
-import kotlinx.android.parcel.Parcelize
-import kotlinx.android.synthetic.main.fragment_main.text_view
+import kotlinx.android.synthetic.main.fragment_main.fragment_main_status_bar
+import kotlinx.android.synthetic.main.include_search_toolbar.include_search_toolbar_back_title_button
+import kotlinx.android.synthetic.main.include_search_toolbar.include_search_toolbar_clear_edit_text_button
+import kotlinx.android.synthetic.main.include_search_toolbar.include_search_toolbar_container
+import kotlinx.android.synthetic.main.include_search_toolbar.include_search_toolbar_edit_text
+import kotlinx.android.synthetic.main.include_search_toolbar.include_search_toolbar_search_button
+import kotlinx.android.synthetic.main.include_search_toolbar.include_search_toolbar_search_group
+import kotlinx.android.synthetic.main.include_search_toolbar.include_search_toolbar_title_group
 import ru.khiraevmalik.theguardiannews.R
-import ru.khiraevmalik.theguardiannews.extensions.args
-import ru.khiraevmalik.theguardiannews.extensions.withParcelable
 import ru.khiraevmalik.theguardiannews.presentation.BaseFragment
+import ru.khiraevmalik.theguardiannews.utils.addSystemTopPadding
+import ru.khiraevmalik.theguardiannews.utils.hideSoftKeyboard
+import ru.khiraevmalik.theguardiannews.utils.rippleClick
+import ru.khiraevmalik.theguardiannews.utils.showSoftKeyboard
+import ru.khiraevmalik.theguardiannews.utils.visible
 
 class NewsListFragment : BaseFragment(R.layout.fragment_main) {
 
     companion object {
-        private const val STATE_KEY = "NewsListFragment"
-        fun newInstance() = NewsListFragment().withParcelable(STATE_KEY, State("Hello"))
+        private const val TOOLBAR_SEARCH_TRANSITION_DELAY = 200L
+        fun newInstance() = NewsListFragment()
     }
 
-    private val state: State by args(STATE_KEY)
+    private val fade: Transition = Fade().apply {
+        duration = TOOLBAR_SEARCH_TRANSITION_DELAY
+        mode = Visibility.MODE_IN
+    }
+
+    private val showKeyboardPostDelayed = Runnable {
+        showSoftKeyboard(include_search_toolbar_edit_text)
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        text_view.text = state.x
-        Log.i(STATE_KEY, "onViewCreated ${this}")
+        setWindowInsetsListener(view)
+        include_search_toolbar_search_button.rippleClick {
+            showOrHideSearchToolbar(true)
+        }
+        include_search_toolbar_back_title_button.rippleClick {
+            showOrHideSearchToolbar(false)
+        }
+        include_search_toolbar_clear_edit_text_button.rippleClick {
+            include_search_toolbar_edit_text.text?.clear()
+        }
+        include_search_toolbar_edit_text.setOnFocusChangeListener { editTextView, hasFocus ->
+            if (!hasFocus) hideSoftKeyboard(editTextView)
+        }
     }
 
-    @Parcelize
-    data class State(val x: String) : Parcelable
+    override fun onStop() {
+        super.onStop()
+        include_search_toolbar_edit_text.removeCallbacks(showKeyboardPostDelayed)
+    }
+
+    private fun setWindowInsetsListener(view: View) {
+        view.addSystemTopPadding(fragment_main_status_bar)
+    }
+
+    private fun showOrHideSearchToolbar(show: Boolean) {
+        TransitionManager.beginDelayedTransition(include_search_toolbar_container, fade)
+        include_search_toolbar_search_group.visible(show)
+        include_search_toolbar_title_group.visible(!show)
+        if (show) focusSearchEditText() else hideSoftKeyboard(include_search_toolbar_edit_text)
+    }
+
+    private fun focusSearchEditText() {
+        include_search_toolbar_edit_text.postDelayed(showKeyboardPostDelayed, TOOLBAR_SEARCH_TRANSITION_DELAY)
+    }
 
 }
